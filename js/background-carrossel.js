30ea6a9b4bd2885852779c450ea3cd2eb5d96102
Eduardo Fagundes da Silva <115734150+EduFag@users.noreply.github.com>
2025-05-07 01:50:36 -0300

    Add files via upload

--- js/background-carrossel.js
+++ js/background-carrossel.js
@@ -1,6 +1,6 @@
 const imagens = [
     'img/fundobatata.jpeg',
-    'https://img.freepik.com/fotos-gratis/embarque-em-seco_1398-242.jpg?t=st=1746041424~exp=1746045024~hmac=7c180ce6f969a1f31e0dedc7617c618319fd2a4fc6bca8f9fcfe0eca9d1f52b7&w=1380',
+    'img/embarque-em-seco.jpg',
     'img/fundo3.avif',
     'img/fundosalmao.jpeg',
     'img/fundo1.jpg'
